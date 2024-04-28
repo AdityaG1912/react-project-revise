d726bf0fecc49df9d63bd429c14effbb4ba3ee84
AdityaG1912 <aditya.gaikwad2013@gmail.com>
2024-04-28 16:13:41 +0530

    theme swither complete

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
+import Button from "./components/Button";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/*theme button*/}
+            <Button></Button>
           </div>
-          <div className="w-full max-w-sm mx-auto">{/*card ui*/}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card></Card>
+          </div>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
